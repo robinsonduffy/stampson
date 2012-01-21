23ef16aa7f80d434ca4fa3c136cc4ed4873ead55
Robinson Duffy <robinson.duffy@gmail.com>
2012-01-21 12:40:54 -0900

    Postgres fix order

--- app/models/country.rb
+++ app/models/country.rb
@@ -6,6 +6,6 @@ class Country < ActiveRecord::Base
                    
   has_many :items, :dependent => :destroy
   
-  scope :active, :include => :items, :conditions => ['items.id IS NOT NULL'], :order => ['(countries.name = "Specials"), countries.name']
-  scope :no_specials, :conditions => ['countries.name != "Specials"'], :order => ['countries.name']
+  scope :active, :include => :items, :conditions => ['items.id IS NOT NULL'], :order => ["(countries.name = 'Specials'), countries.name"]
+  scope :no_specials, :conditions => ["countries.name != 'Specials'"], :order => ['countries.name']
 end
