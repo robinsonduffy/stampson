b69c24d7bc5ba3993c611f03b597e5e57fee5a79
Robinson Duffy <robinson.duffy@gmail.com>
2012-02-04 17:10:17 -0900

    Specials added to navigation

--- app/models/country.rb
+++ app/models/country.rb
@@ -5,7 +5,7 @@ class Country < ActiveRecord::Base
                    :uniqueness => {:case_sensitve => false}
                    
   has_many :items, :dependent => :destroy
-  
+   
   scope :active, :include => :items, :conditions => ['items.id IS NOT NULL'], :order => ["(countries.name = 'Specials'), countries.name"]
   scope :no_specials, :conditions => ["countries.name != 'Specials'"], :order => ['countries.name']
   scope :sell, :include => {:items => :prices}, :conditions => "prices.condition != 'BUY'"
